d6512e52b396af1df89a5333cbb109e9bf96b62f
Edward Do <edwardtdo@gmail.com>
2023-03-06 14:51:01 +1100

    miraculously no type errors on carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, MutableRefObject, ReactComponentElement } from "react";
+import React, { useState, useRef, MutableRefObject } from "react";
 import '!style-loader!css-loader!sass-loader!./carousel.scss'
 import Card from "../Card/Card";
 
@@ -8,7 +8,7 @@ interface ComponentProps {
 }
 
 interface ChildrenProps {
-	children: React.ReactNode;
+	children: React.ReactElement<any, string | React.JSXElementConstructor<any>> | null;
 }
 
 interface CarouselProps extends ChildrenProps {
@@ -17,8 +17,7 @@ interface CarouselProps extends ChildrenProps {
 	items: ComponentProps[];
 }
 
-interface DefaultWrapperProps {
-	children: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+interface DefaultWrapperProps extends ChildrenProps {
 	opacity?: number;
 }
 
@@ -26,61 +25,57 @@ const wrapperStyle: React.CSSProperties = {
 	border: "5px solid red",
 	padding: `var(--spacer-md)`
 };
-const Wrapper = ({ children }: ChildrenProps) => <div style={wrapperStyle}>{children}</div>
 
-const DefaultWrapper = ({ children, opacity = 0.5 }: DefaultWrapperProps) =>
-	React.Children.map(children, (child: React.ReactElement) => (
-		<Wrapper>
-			{React.cloneElement(child, {
-				style: { ...child.props.style, opacity: opacity }
-			})}
-		</Wrapper>
-	));
+const Wrapper = ({ children }: { children: React.ReactNode }) => (
+  <div style={wrapperStyle}>{children}</div>
+);
 
-export default function Carousel( { children, ...props }: CarouselProps ) { 
-	const { 
-		heading,
-		subheading,
-		items
-	} = props;
-	const [gradient, setGradient] = useState('right');
-	const carouselEl: HTMLDivElement | MutableRefObject<any> = useRef();
-	const handleScroll = (e: React.BaseSyntheticEvent) => {
-		if (e.target === document) return;
-		const scrollLeft = e.target.scrollLeft;
-		const fullWidth = e.target.scrollWidth - e.target.offsetWidth;
+const DefaultWrapper = ({ children, opacity = 0.5 }: DefaultWrapperProps) => (
+  <>
+    {React.Children.map(children, (child) => (
+      <Wrapper>
+        {React.cloneElement(child as React.ReactElement, {
+          style: { ...child!.props.style, opacity: opacity },
+        })}
+      </Wrapper>
+    ))}
+  </>
+);
 
-		if (scrollLeft === fullWidth) {
-			setGradient('left');
-			return
-		}
-		if (scrollLeft === 0) {
-			setGradient('right');
-		} else {
-			setGradient('full');
-		} 
-	}
-	return (
-		<div className="container mds__component">
-			{ heading && 
-				<h2 className="heading">
-					{ heading }
-				</h2>
-			}
-			{ subheading && 
-				<p className="subheading">
-					{ subheading }
-				</p>
-			}
-			{ items.length && 
-				<div className="carousel-container">
-					<div ref={carouselEl} className={`horizontal-scroll ${gradient}`} onScroll={handleScroll}>
-						<DefaultWrapper>
-							{ children }
-						</DefaultWrapper>
-					</div>
-				</div>
-			}
-		</div>
-	);
-};
+export default function Carousel({ children, ...props }: CarouselProps) {
+  const { heading, subheading, items } = props;
+  const [gradient, setGradient] = useState("right");
+  const carouselEl: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+  const handleScroll = (e: React.BaseSyntheticEvent) => {
+    if (e.target === document) return;
+    const scrollLeft = e.target.scrollLeft;
+    const fullWidth = e.target.scrollWidth - e.target.offsetWidth;
+
+    if (scrollLeft === fullWidth) {
+      setGradient("left");
+    } else if (scrollLeft === 0) {
+      setGradient("right");
+    } else {
+      setGradient("full");
+    }
+  };
+
+  return (
+    <div className="container mds__component">
+      {heading && <h2 className="heading">{heading}</h2>}
+      {subheading && <p className="subheading">{subheading}</p>}
+      {items.length > 0 && (
+        <div className="carousel-container">
+          <div
+            ref={carouselEl}
+            className={`horizontal-scroll ${gradient}`}
+            onScroll={handleScroll}
+          >
+            <DefaultWrapper>{children}</DefaultWrapper>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
